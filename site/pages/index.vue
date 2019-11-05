b2792c444bc395997f1f6866b70de0b79cd65f4e
Tomas Miciak <tomas.miciak@gmail.com>
2019-11-04 21:39:28 -0800

    Updated styles

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="container">
-    <img src="~/assets/coin-story.svg">
-    <vue-markdown :source="MarkdownSource" />
+  <div class="page">
+    <section>
+        <div class="container">
+            <div class="title">
+                <a href="http://coinstory.tech"><img src="~/assets/img-coin-story.png"></a>
+                <div>Your shortcut to the crypto world</div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <vue-markdown :source="MarkdownSource" />
+        </div>
+    </section>
   </div>
 </template>
 
